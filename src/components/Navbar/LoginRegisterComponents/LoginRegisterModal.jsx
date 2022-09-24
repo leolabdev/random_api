110c05eff7374d2d0735015de2f1fe67130f07b8
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-09-24 18:28:42 +0300

    added modalpage for login/register

---
+++ src/components/Navbar/LoginRegisterComponents/LoginRegisterModal.jsx
@@ -0,0 +1,148 @@
+import React, {useState} from 'react';
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+
+
+
+
+const LoginRegisterModal = (props) => {
+
+    let [authMode, setAuthMode] = useState("signin")
+
+    const changeAuthMode = () => {
+        setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+
+    return (
+        <div>
+            {
+                authMode === "signin"
+                    ?
+                    <Modal
+                        {...props}
+                        size="lg"
+                        aria-labelledby="contained-modal-title-vcenter"
+                        centered
+                        backdrop="static"
+                    >
+                        <Modal.Header className='text-center'>
+                            {/*<Modal.Title   id="contained-modal-title-vcenter " >*/}
+                            {/*    */}
+                            {/*</Modal.Title>*/}
+                            <h3 className="modal-title w-100 ">Sign In</h3>
+                        </Modal.Header>
+                        <div className="text-center">
+                            Not registered yet?{" "}
+                            <span className="link-primary" onClick={changeAuthMode} style={{cursor:'pointer'}}>
+                            Sign Up
+                        </span>
+                        </div>
+                        <Modal.Body>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="text" placeholder="Enter username" onChange={(event)=>{
+                                    event.preventDefault();
+                                    props.setUsernameLogin(event.target.value);
+                                }}/>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" onChange={(event)=>{
+                                    event.preventDefault();
+                                    props.setPasswordLogin(event.target.value);
+                                }}/>
+                            </Form.Group>
+                            <Form.Text className="text-muted">
+                                {props.statusLogin}
+                            </Form.Text>
+                            {/*<h4>Centered Modal</h4>*/}
+                            {/*<p>*/}
+                            {/*    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,*/}
+                            {/*    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac*/}
+                            {/*    consectetur ac, vestibulum at eros.*/}
+                            {/*</p>*/}
+
+                        </Modal.Body>
+                        <Modal.Footer style={{display:'block'}}>
+                            {/*<Button onClick={props.onHide}>Close</Button>*/}
+                            <div className='d-grid gap-2'>
+                                <Button variant="primary" type="submit"  onClick={props.loginUser}>
+                                    Submit
+                                </Button>
+                            </div>
+
+                        </Modal.Footer>
+                    </Modal>
+                    :
+                    <Modal
+                        {...props}
+                        size="lg"
+                        aria-labelledby="contained-modal-title-vcenter"
+                        centered
+                        backdrop="static"
+                    >
+                        <Modal.Header className='text-center'>
+                            {/*<Modal.Title   id="contained-modal-title-vcenter " >*/}
+                            {/*    */}
+                            {/*</Modal.Title>*/}
+                            <h3 className="modal-title w-100 ">Sign Up</h3>
+                        </Modal.Header>
+                        <div className="text-center">
+                            Already registered?{" "}
+                            <span className="link-primary" onClick={changeAuthMode} style={{cursor:'pointer'}}>
+                            Sign In
+                             </span>
+                        </div>
+                        <Modal.Body>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="text" placeholder="Enter username" onChange={(event)=>{
+                                    event.preventDefault();
+                                    props.setUsernameRegister(event.target.value);
+                                }}/>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" onChange={(event)=>{
+                                    event.preventDefault();
+                                    props.setPasswordRegister(event.target.value);
+                                }}/>
+                            </Form.Group>
+                            <Form.Text className="text-muted">
+                                {props.statusRegister}
+                            </Form.Text>
+                            {/*<h4>Centered Modal</h4>*/}
+                            {/*<p>*/}
+                            {/*    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,*/}
+                            {/*    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac*/}
+                            {/*    consectetur ac, vestibulum at eros.*/}
+                            {/*</p>*/}
+
+                        </Modal.Body>
+                        <Modal.Footer style={{display:'block'}}>
+                            {/*<Button onClick={props.onHide}>Close</Button>*/}
+                            {/*<div className='d-grid gap-2'>*/}
+                            {/*    <Button variant="primary" type="submit"  onClick={props.loginUser}>*/}
+                            {/*        Submit*/}
+                            {/*    </Button>*/}
+                            {/*</div>*/}
+                            <div className='d-grid gap-2'>
+                                <Button id="registerButton" variant="primary" type="submit" onClick={props.registerUser}>
+                                    Submit
+                                </Button>
+                            </div>
+
+                        </Modal.Footer>
+                    </Modal>
+
+            }
+
+        </div>
+    );
+};
+
+export default LoginRegisterModal;
--- src/pages/LoginRegisterPage.jsx
+++ src/pages/LoginRegisterPage.jsx
@@ -1,6 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {useState} from "react";
+import LoginRegisterModal from "../components/Navbar/LoginRegisterComponents/LoginRegisterModal";
 
 function LoginRegisterPage(props) {
     const [usernameLogin, setUsernameLogin] = useState('');
@@ -18,6 +19,8 @@ function LoginRegisterPage(props) {
     const changeAuthMode = () => {
         setAuthMode(authMode === "signin" ? "signup" : "signin")
     }
+
+    const [modalShow, setModalShow] = useState(true);
 
     const loginUser = async (event) => {
         event.preventDefault();
@@ -67,93 +70,112 @@ function LoginRegisterPage(props) {
     }
 
     return (
-        <div id="container">
-            <br/>
-            {
-                authMode === "signin"
-                    ?
-                    <Form centered>
-                        {/*<Form.Text  className="text-muted">*/}
-                        {/*    Sign In*/}
-                        {/*</Form.Text>*/}
-                        <h3 className="text-center">Sign In</h3>
+            <div>
+                <LoginRegisterModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
 
+                    setUsernameLogin={setUsernameLogin}
+                    setPasswordLogin={setPasswordLogin}
 
-                        <div className="text-center">
-                            Not registered yet?{" "}
-                            <span className="link-primary" onClick={changeAuthMode} style={{cursor:'pointer'}}>
-                            Sign Up
-                             </span>
-                        </div>
+                    setUsernameRegister={setUsernameRegister}
+                    setPasswordRegister={setPasswordRegister}
 
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter username" onChange={(event)=>{
-                                event.preventDefault();
-                                setUsernameLogin(event.target.value);
-                            }}/>
-                        </Form.Group>
+                    loginUser = {loginUser}
+                    registerUser={registerUser}
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" onChange={(event)=>{
-                                event.preventDefault();
-                                setPasswordLogin(event.target.value);
-                            }}/>
-                        </Form.Group>
-                        <div className='d-grid gap-2'>
-                            <Button variant="primary" type="submit"  onClick={loginUser}>
-                                Submit
-                            </Button>
-                        </div>
-                        <br/>
-                        <Form.Text className="text-muted">
-                            {statusLogin}
-                        </Form.Text>
-                    </Form>
-                    :
-                    <Form>
+                    statusLogin={statusLogin}
+                    statusRegister={statusRegister}
 
-                        <h3 className="text-center">Sign Up</h3>
-                        <div className="text-center">
-                            Already registered?{" "}
-                            <span className="link-primary" onClick={changeAuthMode} style={{cursor:'pointer'}}>
-                            Sign In
-                             </span>
-                        </div>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter username" onChange={(event)=>{
-                                event.preventDefault();
-                                setUsernameRegister(event.target.value);
-                            }}/>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" onChange={(event)=>{
-                                event.preventDefault();
-                                setPasswordRegister(event.target.value);
-                            }}/>
-                        </Form.Group>
-
-                        <div className='d-grid gap-2'>
-                            <Button id="registerButton" variant="primary" type="submit" onClick={registerUser}>
-                                Submit
-                            </Button>
-                        </div>
-
-
-                        <br/>
-                        <Form.Text className="text-muted">
-                            {statusRegister}
-                        </Form.Text>
-                    </Form>
-            }
-            {/*<br/>*/}
-            {/*<div>OR</div>*/}
-            {/*<br/>*/}
-        </div>
+                />
+            </div>
+        // <div id="container">
+        //     <br/>
+        //     {
+        //         authMode === "signin"
+        //             ?
+        //             <Form centered>
+        //                 {/*<Form.Text  className="text-muted">*/}
+        //                 {/*    Sign In*/}
+        //                 {/*</Form.Text>*/}
+        //                 <h3 className="text-center">Sign In</h3>
+        //
+        //
+        //                 <div className="text-center">
+        //                     Not registered yet?{" "}
+        //                     <span className="link-primary" onClick={changeAuthMode} style={{cursor:'pointer'}}>
+        //                     Sign Up
+        //                      </span>
+        //                 </div>
+        //
+        //                 <Form.Group className="mb-3" controlId="formBasicEmail">
+        //                     <Form.Label>Username</Form.Label>
+        //                     <Form.Control type="text" placeholder="Enter username" onChange={(event)=>{
+        //                         event.preventDefault();
+        //                         setUsernameLogin(event.target.value);
+        //                     }}/>
+        //                 </Form.Group>
+        //
+        //                 <Form.Group className="mb-3" controlId="formBasicPassword">
+        //                     <Form.Label>Password</Form.Label>
+        //                     <Form.Control type="password" placeholder="Password" onChange={(event)=>{
+        //                         event.preventDefault();
+        //                         setPasswordLogin(event.target.value);
+        //                     }}/>
+        //                 </Form.Group>
+        //                 <div className='d-grid gap-2'>
+        //                     <Button variant="primary" type="submit"  onClick={loginUser}>
+        //                         Submit
+        //                     </Button>
+        //                 </div>
+        //                 <br/>
+        //                 <Form.Text className="text-muted">
+        //                     {statusLogin}
+        //                 </Form.Text>
+        //             </Form>
+        //             :
+        //             <Form>
+        //
+        //                 <h3 className="text-center">Sign Up</h3>
+        //                 <div className="text-center">
+        //                     Already registered?{" "}
+        //                     <span className="link-primary" onClick={changeAuthMode} style={{cursor:'pointer'}}>
+        //                     Sign In
+        //                      </span>
+        //                 </div>
+        //                 <Form.Group className="mb-3" controlId="formBasicEmail">
+        //                     <Form.Label>Username</Form.Label>
+        //                     <Form.Control type="text" placeholder="Enter username" onChange={(event)=>{
+        //                         event.preventDefault();
+        //                         setUsernameRegister(event.target.value);
+        //                     }}/>
+        //                 </Form.Group>
+        //
+        //                 <Form.Group className="mb-3" controlId="formBasicPassword">
+        //                     <Form.Label>Password</Form.Label>
+        //                     <Form.Control type="password" placeholder="Password" onChange={(event)=>{
+        //                         event.preventDefault();
+        //                         setPasswordRegister(event.target.value);
+        //                     }}/>
+        //                 </Form.Group>
+        //
+        //                 <div className='d-grid gap-2'>
+        //                     <Button id="registerButton" variant="primary" type="submit" onClick={registerUser}>
+        //                         Submit
+        //                     </Button>
+        //                 </div>
+        //
+        //
+        //                 <br/>
+        //                 <Form.Text className="text-muted">
+        //                     {statusRegister}
+        //                 </Form.Text>
+        //             </Form>
+        //     }
+        //     {/*<br/>*/}
+        //     {/*<div>OR</div>*/}
+        //     {/*<br/>*/}
+        // </div>
     );
 }
 
