2eba98607c17f3c9a7ffd6a49c524fd9db533d38
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-09-29 19:04:46 +0300

    added possibility to delete message from page

--- src/components/ControlAccess/messages/Messages.jsx
+++ src/components/ControlAccess/messages/Messages.jsx
@@ -1,15 +1,29 @@
 import {ListGroup, ListGroupItem} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import {useState} from "react";
 
 const Messages = () => {
 
-    const messages = [
+
+
+    let [messages,setMessages] = useState([
         {id : 1, username : "petya" , message : "hello i am petya" },
         {id : 2, username : "vasya" , message : "hello i am vasya" },
         {id : 3, username : "misha" , message : "hello i am misha" },
         {id : 4, username : "leo" , message : "hello i am leo" },
-        {id : 5, username : "andrew" , message : "hello i am andrew" },
-    ]
+        {id : 5, username : "andrew" , message : "hello i am andrew" }]);
+
+
+    const declineAccess = (id) => {
+        alert("plz implement decline")
+        setMessages(current => current.filter(message => message.id != id))
+    }
+
+    const confirmAccess = (id) => {
+        alert("plz implement confirm")
+        setMessages(current => current.filter(message => message.id != id))
+    }
+
 
     return (
 
@@ -22,8 +36,8 @@ const Messages = () => {
                            id : {m.id} , username: {m.username}, message: {m.message}
                            <br/><br/>
                            <div className="mb-2">
-                               <Button variant="success">Confirm</Button>{' '}
-                               <Button variant="danger">Decline</Button>
+                               <Button variant="success" onClick={() =>confirmAccess(m.id)}>Confirm</Button>{' '}
+                               <Button variant="danger" onClick={() => declineAccess(m.id)}>Decline</Button>
                            </div>
                        </ListGroupItem>
                         <br/>
