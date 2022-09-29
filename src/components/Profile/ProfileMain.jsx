d6a5a766042b76a305d10ed20d836d6f0363c46a
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-09-29 19:07:08 +0300

    removed tokens

--- src/components/Profile/ProfileMain.jsx
+++ src/components/Profile/ProfileMain.jsx
@@ -1,12 +1,12 @@
-import UserTokens from "./UserTokens";
+// import UserTokens from "./UserTokens";
 import Tables from "./Tables";
 import UserHasAccessTables from "./UserHasAccessTables";
 
 const ProfileMain = (props) => {
     return (
         <div id="container">
-            <br/>
-            <UserTokens></UserTokens>
+
+            {/*<UserTokens></UserTokens>*/}
             <br/>
 
             <Tables setTableInfoName={props.setTableInfoName} setTableInfoOwner={props.setTableInfoOwner}></Tables>
