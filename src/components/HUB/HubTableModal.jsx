7f27e193864fe4b22f7dc0252a0ac58afac86187
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-09-29 19:58:06 +0300

    added hardcoded variant of tablemodal

---
+++ src/components/HUB/HubTableModal.jsx
@@ -0,0 +1,39 @@
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import {FormText} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import React from "react";
+import ModalContext from "react-bootstrap/ModalContext";
+
+const HubTableModal = ({table ,show , handleClose}) => {
+    return (
+        <div>
+            <Modal show={show} onHide={handleClose}  size="lg"
+                   aria-labelledby="contained-modal-title-vcenter"
+                   centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>{table.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <FormText>Desctiption: {table.description}</FormText>
+                    <br/>
+                    Owner : {table.username}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    {
+                        table.accessType == 1 ? <Button onClick={()=> alert ("implement me")} variant="success">Send Access Request</Button>
+                            : <Button onClick={()=> alert ("implement me")} variant="success">Add to own collection</Button>
+                    }
+                    {/*<Button variant="success" onClick={()=> alert ("implement me")}>*/}
+                    {/*    Yes*/}
+                    {/*</Button>*/}
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+};
+
+export default HubTableModal;
--- src/components/HUB/HubTables.jsx
+++ src/components/HUB/HubTables.jsx
@@ -1,29 +1,47 @@
 import {Card} from "react-bootstrap";
-import React from "react";
+import React, {useState} from "react";
+import HubTableModal from "./HubTableModal";
 
 const HubTables = ({hubTables}) => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = (modalTable) =>{
+
+        setModalTable(modalTable)
+        setShow(true);
+    }
+
+    const [modalTable, setModalTable] = useState('')
+
+
     return (
         <div className="container d-flex flex-wrap justify-content-center">
             {hubTables.map((hubT) => (
-                <Card
-                    onClick={()=>console.log(hubTables)}
-                    key={hubT.id}
-                    style={{ width: "25rem", margin: "2rem", textAlign: "center",cursor:'pointer' }}
-                >
-                    <Card.Body style = {hubT.accessType == "0" ?  {backgroundColor: '#ADD8E6'} : hubT.accessType == "1" ? {backgroundColor: '#FFE898' } : {backgroundColor: '#90EE90'}}>
-                        <Card.Title>
-                            {" "}
-                            <span style={{ fontWeight: "bold" }}>Table:</span> {hubT.name}
-                        </Card.Title>
-                        <Card.Text>
-                            <span className='d-inline-block text-truncate' style={{maxWidth : '250px' ,fontWeight :'bold'}}>Description: {hubT.description}</span>
-                        </Card.Text>
-                        <Card.Text>
-                            <span style={{ fontWeight: "bold" }}>Owner:</span> {hubT.username}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                <div>
+                    <Card
+                        onClick={() => handleShow(hubT)}
+                        key={hubT.id}
+                        style={{ width: "25rem", margin: "2rem", textAlign: "center",cursor:'pointer' }}
+                    >
+                        <Card.Body style = {hubT.accessType == "0" ?  {backgroundColor: '#ADD8E6'} : hubT.accessType == "1" ? {backgroundColor: '#FFE898' } : {backgroundColor: '#90EE90'}}>
+                            <Card.Title>
+                                {" "}
+                                <span style={{ fontWeight: "bold" }}>Table:</span> {hubT.name}
+                            </Card.Title>
+                            <Card.Text>
+                                <span className='d-inline-block text-truncate' style={{maxWidth : '250px' ,fontWeight :'bold'}}>Description: {hubT.description}</span>
+                            </Card.Text>
+                            <Card.Text>
+                                <span style={{ fontWeight: "bold" }}>Owner:</span> {hubT.username}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+
             ))}
+            <HubTableModal table={modalTable} handleClose={handleClose} show={show}/>
         </div>
     );
 };
