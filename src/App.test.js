a611480c7df263c256904a0abc630d1be89747e5
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-09-29 19:58:54 +0300

    added new path

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sign In/i);
   expect(linkElement).toBeInTheDocument();
 });
