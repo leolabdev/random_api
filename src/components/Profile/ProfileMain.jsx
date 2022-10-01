cb039d4586ee6df2c03f0a72e02485f9228959cc
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-10-01 14:58:50 +0300

    added colors for tables

--- src/components/Profile/ProfileMain.jsx
+++ src/components/Profile/ProfileMain.jsx
@@ -10,12 +10,13 @@ const ProfileMain = (props) => {
             <br/>
             {/*<UserTokens></UserTokens>*/}
             <div  className='d-flex justify-content-left gap-3'
-                  style={{textAlign : 'left',backgroundColor: '',width:'380px'}}
+                  style={{textAlign : 'center',backgroundColor: '',width:''}}
             >
                 <div style={{color: "#ADD8E6", fontWeight: '900'}}>&#9632; <span>Private</span></div>
                 {/*<div style={{color: "#FFE898",fontWeight: '900'}}>&#9632; <span>Access required</span></div>*/}
-                <div style={{color: "#FFD580",fontWeight: '900'}}>&#9632; <span>Access required</span></div>
+                <div style={{color: "#FFD580",fontWeight: '900'}}>&#9632; <span>Required</span></div>
                 <div style={{color: "#90EE90",fontWeight: '900'}}>&#9632; <span>Public</span></div>
+                <div style={{color: "#FFB6C1",fontWeight: '900'}}>&#9632; <span>Permitted</span></div>
             </div>
             <br/>
 
