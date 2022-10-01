dd33ef4846f54802c02777ce5a5fc4fd0a93eaf8
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-10-01 21:56:35 +0300

    added base of get messages  (requested dont work yet)

--- src/components/ControlAccess/messages/Messages.jsx
+++ src/components/ControlAccess/messages/Messages.jsx
@@ -1,17 +1,41 @@
 import {ListGroup, ListGroupItem} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const Messages = () => {
 
+    const apiBasePath = `http://${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`;
 
 
     let [messages,setMessages] = useState([
-        {id : 1, username : "petya" , message : "hello i am petya" },
-        {id : 2, username : "vasya" , message : "hello i am vasya" },
-        {id : 3, username : "misha" , message : "hello i am misha" },
-        {id : 4, username : "leo" , message : "hello i am leo" },
-        {id : 5, username : "andrew" , message : "hello i am andrew" }]);
+        { id: 7, sender: "test", receiver: "mike", tableName: "cars", message: null },
+        // {id : 1, username : "petya" , message : "hello i am petya" },
+        // {id : 2, username : "vasya" , message : "hello i am vasya" },
+        // {id : 3, username : "misha" , message : "hello i am misha" },
+        // {id : 4, username : "leo" , message : "hello i am leo" },
+        // {id : 5, username : "andrew" , message : "hello i am andrew" }]);
+]);
+
+
+    useEffect(  () => {
+        fetchAccessRequests();
+    }, []);
+
+    const fetchAccessRequests = async () => {
+        const reqOptions = {
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            method: 'GET',
+            credentials: 'include'
+        }
+
+        const resp = await fetch(`${apiBasePath}/accessRequest`, reqOptions);
+        const respJson = await resp.json();
+        setMessages(respJson.result);
+        console.log(respJson.result)
+    }
+
 
 
     const declineAccess = (id) => {
@@ -29,11 +53,12 @@ const Messages = () => {
 
         <div>
             {
+                (messages[0] != undefined) &&
                 messages.map((m)=> (
 
                     <ListGroup>
                        <ListGroupItem>
-                           id : {m.id} , username: {m.username}, message: {m.message}
+                           id : {m.id} , tableName : {m.tableName}, sender: {m.sender}, message: {m.message}
                            <br/><br/>
                            <div className="mb-2">
                                <Button variant="success" onClick={() =>confirmAccess(m.id)}>Confirm</Button>{' '}
