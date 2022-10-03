d492fd6501277fb29b043551c9c28d7a69c9746e
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-10-03 20:18:46 +0300

    deleted external code

--- src/components/LoginRegisterComponents/LoginRegisterModal.jsx
+++ src/components/LoginRegisterComponents/LoginRegisterModal.jsx
@@ -4,9 +4,6 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 
-
-
-
 const LoginRegisterModal = (props) => {
 
     let [authMode, setAuthMode] = useState("signin")
@@ -28,9 +25,6 @@ const LoginRegisterModal = (props) => {
                         backdrop="static"
                     >
                         <Modal.Header className='text-center'>
-                            {/*<Modal.Title   id="contained-modal-title-vcenter " >*/}
-                            {/*    */}
-                            {/*</Modal.Title>*/}
                             <h3 className="modal-title w-100 ">Sign In</h3>
                         </Modal.Header>
                         <div className="text-center">
@@ -42,7 +36,7 @@ const LoginRegisterModal = (props) => {
                         <Modal.Body>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control type="text" placeholder="Enter username" onChange={(event)=>{
+                                <Form.Control type="text" placeholder="Enter username"  onChange={(event)=>{
                                     event.preventDefault();
                                     props.setUsernameLogin(event.target.value);
                                 }}/>
@@ -58,13 +52,6 @@ const LoginRegisterModal = (props) => {
                             <Form.Text className="text-muted">
                                 {props.statusLogin}
                             </Form.Text>
-                            {/*<h4>Centered Modal</h4>*/}
-                            {/*<p>*/}
-                            {/*    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,*/}
-                            {/*    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac*/}
-                            {/*    consectetur ac, vestibulum at eros.*/}
-                            {/*</p>*/}
-
                         </Modal.Body>
                         <Modal.Footer style={{display:'block'}}>
                             {/*<Button onClick={props.onHide}>Close</Button>*/}
@@ -85,9 +72,6 @@ const LoginRegisterModal = (props) => {
                         backdrop="static"
                     >
                         <Modal.Header className='text-center'>
-                            {/*<Modal.Title   id="contained-modal-title-vcenter " >*/}
-                            {/*    */}
-                            {/*</Modal.Title>*/}
                             <h3 className="modal-title w-100 ">Sign Up</h3>
                         </Modal.Header>
                         <div className="text-center">
@@ -99,7 +83,7 @@ const LoginRegisterModal = (props) => {
                         <Modal.Body>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control type="text" placeholder="Enter username " onChange={(event)=>{
+                                <Form.Control type="text" placeholder="Enter username "  onChange={(event)=>{
                                     event.preventDefault();
                                     props.setUsernameRegister(event.target.value);
                                 }}/>
@@ -122,21 +106,8 @@ const LoginRegisterModal = (props) => {
                             <Form.Text className="text-muted">
                                 {props.statusRegister}
                             </Form.Text>
-                            {/*<h4>Centered Modal</h4>*/}
-                            {/*<p>*/}
-                            {/*    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,*/}
-                            {/*    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac*/}
-                            {/*    consectetur ac, vestibulum at eros.*/}
-                            {/*</p>*/}
-
                         </Modal.Body>
                         <Modal.Footer style={{display:'block'}}>
-                            {/*<Button onClick={props.onHide}>Close</Button>*/}
-                            {/*<div className='d-grid gap-2'>*/}
-                            {/*    <Button variant="primary" type="submit"  onClick={props.loginUser}>*/}
-                            {/*        Submit*/}
-                            {/*    </Button>*/}
-                            {/*</div>*/}
                             <div className='d-grid gap-2'>
                                 <Button id="registerButton" variant="primary" type="submit" onClick={props.registerUser}>
                                     Submit
