c59459847197160f421fb90a2ca8cec6d1635890
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-10-03 20:17:44 +0300

    repaired max text length in the cards

--- src/components/HUB/HubTables.jsx
+++ src/components/HUB/HubTables.jsx
@@ -18,23 +18,23 @@ const HubTables = ({hubTables}) => {
 
     return (
         <div className="container d-flex flex-wrap justify-content-center">
-            {hubTables.map((hubT) => (
+            {hubTables.map((hubT,id) => (
                 <div>
                     <Card
                         onClick={() => handleShow(hubT)}
-                        key={hubT.id}
+                        key={hubT.id ? hubT.id : id}
                         style={{ width: "20rem", margin: "2rem", textAlign: "center",cursor:'pointer' }}
                     >
                         <Card.Body style = {hubT.accessType == "0" ?  {backgroundColor: '#90EE90'} : hubT.accessType == "1" ? {backgroundColor: '#FFD580' } : {backgroundColor: '#ADD8E6'}}>
                             <Card.Title>
                                 {" "}
-                                <span style={{ fontWeight: "bold" }}>Table:</span> {hubT.name}
+                                <span className='d-inline-block text-truncate' style={{maxWidth : '250px' ,fontWeight :'bold'}}>Table: {hubT.name}</span>
                             </Card.Title>
                             <Card.Text>
                                 <span className='d-inline-block text-truncate' style={{maxWidth : '250px' ,fontWeight :'bold'}}>Description: {hubT.description}</span>
                             </Card.Text>
                             <Card.Text>
-                                <span style={{ fontWeight: "bold" }}>Owner:</span> {hubT.username}
+                                <span className='d-inline-block text-truncate' style={{maxWidth : '250px' ,fontWeight :'bold'}}>Owner: {hubT.username}</span>
                             </Card.Text>
                         </Card.Body>
                     </Card>
