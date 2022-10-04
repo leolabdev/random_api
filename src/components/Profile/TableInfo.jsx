e2b11a7c54d6f8f5988bba663dc50854a7738675
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-10-04 15:53:30 +0300

    small change

--- src/components/Profile/TableInfo.jsx
+++ src/components/Profile/TableInfo.jsx
@@ -3,7 +3,7 @@ import {useNavigate} from "react-router-dom";
 import TableAccessDescription from "./TableAccessDescription";
 import DeleteTable from "./DeleteTable";
 import UpdateTable from "./UpdateTable";
-import {Button} from "react-bootstrap";
+import {Button, ListGroup} from "react-bootstrap";
 // import DeleteTable from "./deleteTable";
 
 const apiBasePath = `http://${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`;
@@ -12,19 +12,16 @@ function TableInfo(props) {
 
     const navigate = useNavigate();
 
-    const [tableName, setTableName] = useState('');
-    const [tableDescription, setTableDescription] = useState('');
-    const [tableOwner, setTableOwner] = useState('');
-    const [tableAccessType, setTableAccessType] = useState('');
-    const [tableId, setTableId] = useState('');
+    const [table,setTable] = useState({});
 
     const [updateMode,setUpdateMode] = useState(false);
 
     useEffect(() => {
         fetchTableData();
-    });
+    },[]);
 
-    const fetchTableData = async () => {
+    const fetchTableData = async (e) => {
+        // e.stopPropagation();
         const reqOptions = {
             headers:{
                 'Content-Type': 'application/json'
@@ -33,55 +30,67 @@ function TableInfo(props) {
             credentials: 'include'
         }
 
-        const resp = await fetch(`${apiBasePath}/userDatabase/${props.name}?owner=${props.owner}`, reqOptions);
+        const resp = await fetch(`${apiBasePath}/userDatabase/${props.name}?owner=${props.owner}&includingElements=true`, reqOptions);
         const respJson = await resp.json();
         const result = respJson.result;
-        setTableName(result.name);
-        setTableDescription(result.description);
-        setTableOwner(result.username);
-        setTableAccessType(result.accessType)
-        setTableId(result.id);
+
+
+        setTable(result);
+
+        console.log(result);
+
     }
 
     return (
-        <div id="tableInfo">
+        <div id="tableInfo" style={{}}>
             <br/>
             <TableAccessDescription/>
 
             <br/>
-            <button onClick={() => navigate(-1)}>Go back</button>
-            <br/><br/>
+            {!updateMode && <div><button onClick={() => navigate('/profile')}>Go back</button><br/><br/></div>  }
+
 
             {updateMode !== true ?
-                <div>
-                    <h3   style={tableAccessType == "0"
+                <div style={{maxWidth:'100%'}}>
+                    <ListGroup style={table?.accessType == "0"
                         ?
-                        {backgroundColor: '#90EE90'}
+                        {backgroundColor: '#90EE90',padding : '20px 0px 20px 20px'}
                         :
-                        tableAccessType == "1"
-                            ? {backgroundColor: '#FFD580' }
-                            : {backgroundColor: '#ADD8E6'}}>{tableName} table information</h3>
+                        table.accessType == "1"
+                            ? {backgroundColor: '#FFD580',padding : '20px' }
+                            : {backgroundColor: '#ADD8E6',padding : '20px'}}>
 
-                    <h4>Description</h4>
-                    <p>{tableDescription}</p>
+                    <h3> table information</h3>
+
+                    <h4 >Description</h4>
+                    <p>{table.description && table.description}</p>
 
                     <h4>Owner</h4>
-                    <p>{tableOwner}</p>
+                    <p>{table.username && table.username}</p>
 
+                        <div className="elements" style={{maxHeight : "200px"}}>
+                            <h4>Table Elements</h4>
+
+                            {table.elements && table.elements.map((element,id) => (
+                                <ul key={id}>
+                                    <li key={id}>{element.value}</li>
+                                </ul>
+                            ))}
+                        </div>
+                    <br/>
                     <h3>Update Table</h3>
-                    <Button onClick={()=>setUpdateMode(!updateMode)}>Update</Button>
-                    <br/>
+                    <Button style={{maxWidth:'80px'}} onClick={()=>setUpdateMode(!updateMode)}>Update</Button>
+
                     <br/>
 
-                    <DeleteTable tableName={tableName}/>
+                    <DeleteTable tableName={table.name}/>
+                    </ListGroup>
                 </div>
                 :
                 <div>
                     <UpdateTable
-                        updateMode={updateMode}
+                        table={table}
                         setUpdateMode={setUpdateMode}
-                        tableName={tableName}
-                        tableOwner={tableOwner}
                     />
                 </div>
             }
